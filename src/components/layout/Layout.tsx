1c1812794e3883684dee52a7bc2649d8e7165a06
[dyad] <jeffcleds@yahoo.com.ph>
2025-10-07 21:07:12 +0800

    [dyad] Added particles background to all pages - wrote 4 file(s), added tsparticles package(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ParticlesBackground from "../animations/ParticlesBackground"; // Import the new ParticlesBackground component
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,9 +9,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col"> {/* Added relative to contain fixed particles */}
+      <ParticlesBackground /> {/* Add the particles background */}
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 relative z-0">{children}</main> {/* Added relative z-0 to ensure content is above particles */}
       <Footer />
     </div>
   );
