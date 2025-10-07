14e951c500f83e1a68806a0244129b47c4d232f5
[dyad] <jeffcleds@yahoo.com.ph>
2025-10-07 21:10:39 +0800

    [dyad] Replaced tsparticles with custom OGL particle system - wrote 5 file(s), deleted 1 file(s), added ogl package(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import ParticlesBackground from "../animations/ParticlesBackground"; // Import the new ParticlesBackground component
+import Particles from "../animations/Particles"; // Import the new Particles component
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,7 +10,18 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative flex min-h-screen flex-col"> {/* Added relative to contain fixed particles */}
-      <ParticlesBackground /> {/* Add the particles background */}
+      <div style={{ width: '100%', height: '100%', position: 'fixed', top: 0, left: 0, zIndex: -1 }}>
+        <Particles
+          particleColors={['#ffffff', '#ffffff']}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
       <Navbar />
       <main className="flex-1 relative z-0">{children}</main> {/* Added relative z-0 to ensure content is above particles */}
       <Footer />
