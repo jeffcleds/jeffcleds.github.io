dc4294ae5fb3d92dfdff21b7f3d377415cb9affc
[dyad] <jeffcleds@yahoo.com.ph>
2025-10-07 21:35:27 +0800

    Reverted all changes back to version 381864863608e03ec6724fd78cf14a576581c85f

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import LiquidBackground from "../animations/LiquidBackground"; // Import the new component
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,12 +8,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative flex min-h-screen flex-col overflow-hidden"> {/* Added relative and overflow-hidden */}
-      <LiquidBackground /> {/* Render the background component */}
+    <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1 relative z-10"> {/* Added relative z-10 to ensure content is above background */}
-        {children}
-      </main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
