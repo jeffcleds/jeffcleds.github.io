e7540500b7e42ba21bfad13d2a2b29eeccaffb4c
[dyad] <jeffcleds@yahoo.com.ph>
2025-10-07 12:02:11 +0800

    Reverted all changes back to version 34502e827b31bb15eaf031970c21261e643241e1

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/layout/ThemeProvider.tsx"; // Import ThemeProvider
 
-console.log("main.tsx: Rendering App with ThemeProvider"); // Added console log
-
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <App />
